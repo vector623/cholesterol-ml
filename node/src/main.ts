f0dbf295b30eea24fd83da59fe0b7ad8e8291131
David G <dg@xounges.net>
2025-01-25 00:11:26 -0500

    ocr over set
    todo: write repository to persist raw text output with image
    filename

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,13 +1,39 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import {ollamaOCR, DEFAULT_OCR_SYSTEM_PROMPT, LlamaOCRConfig} from "ollama-ocr";
+import pLimit from 'p-limit';
 
-async function runOCR() {
+async function runOCR(filePath: string): Promise<string> {
     const config: LlamaOCRConfig = {
         model: "minicpm-v",
-        filePath: "/home/davidg/gits/llm-sandbox/data/iCloud Photos/IMG_4520.JPEG",
+        filePath: filePath,
         systemPrompt: DEFAULT_OCR_SYSTEM_PROMPT,
     };
     const text = await ollamaOCR(config);
-    console.log(text);
+    return text;
 }
 
-runOCR();
+console.log(process.cwd());
+
+
+(async () => {
+    const dataDir = "/home/davidg/gits/cholesterol-ml/data/iCloud Photos/";
+    const files = fs.readdirSync(dataDir)
+        .slice(0, 2)
+        .map(file => path.join(dataDir, file));
+
+    const limit = pLimit(1);
+    const resultsPromises = files
+        .map(async file => {
+            return await limit(async () => {
+                let result: any = {
+                    image: file,
+                    text: await runOCR(file)
+                };
+                return result;
+            });
+        });
+    const results = await Promise.all(resultsPromises);
+
+    console.log(results);
+})();
