40cf75d58f2d673ae611054ccb4471568f9ea023
David G <dg@xounges.net>
2025-02-13 22:33:29 -0500

    save changes

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,19 +1,26 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import {ollamaOCR, DEFAULT_OCR_SYSTEM_PROMPT, LlamaOCRConfig} from "ollama-ocr";
 import pLimit from 'p-limit';
-import {OcrResult, saveToJSON} from "./data_repo";
-import {prompts} from "./prompts";
-import {pipe} from "fp-ts/function";
-import * as TE from "fp-ts/TaskEither";
-import {askOllama} from './data_factory';
+import {OcrResult} from "./data_repo";
 
+async function runOCR(filePath: string): Promise<string> {
+    const config: LlamaOCRConfig = {
+        model: "minicpm-v",
+        filePath: filePath,
+        systemPrompt: DEFAULT_OCR_SYSTEM_PROMPT,
+    };
+    const text = await ollamaOCR(config);
+    return text;
+}
 
-console.log(prompts.jsonv2);
+console.log(process.cwd());
+
 
 (async () => {
-    const dataDir = "/home/davidg/gits/cholesterol-ml/data/iCloud Photos/";
+    const dataDir = "/home/davidg/gits/cholesterol-ml/node/data/cholesterol-data/";
     const files = fs.readdirSync(dataDir)
-        .slice(0, 1)
+        .slice(0, 2)
         .map(file => path.join(dataDir, file));
 
     const limit = pLimit(1);
@@ -22,25 +29,12 @@ console.log(prompts.jsonv2);
             return await limit(async () => {
                 let result: OcrResult = {
                     filename: file,
-                    text: await askOllama(file)
+                    text: await runOCR(file)
                 };
                 return result;
             });
         });
     const results = await Promise.all(resultsPromises);
-    let outcome = pipe(
-        saveToJSON(results, 'data.json'),
-        TE.fold(
-            (err) => {
-                console.error('Failed to save JSON:', err.message);
-                return (): Promise<void> => Promise.resolve(); // or handle the error appropriately
-            },
-            () => {
-                console.log('JSON file has been saved successfully.');
-                return (): Promise<void> => Promise.resolve(); // or perform another action
-            }
-        )
-    )();
 
     console.log(results);
 })();
