b5bbde5b1cd9f78ed093210fc459ca2ee331e402
David G <dg@xounges.net>
2025-02-13 23:29:34 -0500

    refactor to other files

--- node/src/main.ts
+++ node/src/main.ts
@@ -2,40 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import pLimit from 'p-limit';
 import {OcrResult} from "./data_repo";
-import {Ollama} from "ollama";
-
-
-export function encodeImage(imagePath: string): string {
-    const imageFile = fs.readFileSync(imagePath);
-    return Buffer.from(imageFile).toString('base64');
-}
-
-const prompt = `Please look at this image and extract all the text content. Structure the output as JSON with these guidelines:
-    - Identify different sections or components
-    - Use appropriate keys for different text elements
-    - Maintain the hierarchical structure of the content
-    - Include all visible text from the image
-    Provide only the json without any additional comments.
-    `;
-
-export async function askOllama(filePath: string): Promise<string> {
-    const model = 'minicpm-v';
-    const base64Image = encodeImage(filePath);
-    const ollama = new Ollama({ host: 'http://dgp5000:11434' })
-    const response = await ollama.chat({
-        model,
-        options: {},
-        messages: [
-            {
-                role: 'user',
-                content: prompt,
-                images: [base64Image],
-            },
-        ],
-    });
-
-    return response.message.content;
-}
+import {askOllama} from "./data_factory";
 
 (async () => {
     const dataDir = "/home/davidg/gits/cholesterol-ml/node/data/cholesterol-data/";
---
+++ node/src/prompt.ts
@@ -0,0 +1,7 @@
+export const prompt = `Please look at this image and extract all the text content. Structure the output as JSON with these guidelines:
+    - Identify different sections or components
+    - Use appropriate keys for different text elements
+    - Maintain the hierarchical structure of the content
+    - Include all visible text from the image
+    Provide only the json without any additional comments.
+    `;
